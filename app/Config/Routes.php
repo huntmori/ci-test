a9e2ae3843220300f39d2ef3781f81ff849b82a4
huntmori <huntmori04x22@gmail.com>
2026-01-12 21:39:22 +0900

    정적페이지, 뉴스섹션

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Controllers\Pages;
+use App\Controllers\News;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/(:segment)', [News::class, 'show']);
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
+
